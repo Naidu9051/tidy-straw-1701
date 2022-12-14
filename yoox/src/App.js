0da21efc37ea495e6acff6b194fcecfadf619608
Nikhil Naidu <nikhilnaidu703@gmail.com>
2022-12-15 03:53:05 +0530

    added

---
+++ yoox/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Header/Navbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
